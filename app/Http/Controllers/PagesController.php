707f8c05e6e6db80ea73678bd1b6ab019cdfa3fc
Liam O'Reilly <L.P.OReilly@swansea.ac.uk>
2017-02-03 22:02:58 +0000

    Add some comments

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * Controller for static basic pages.
+ */
 class PagesController extends Controller
 {
     public function root()
