93bcf0c3d3ade3d5350b1e021486010258f92b93
Liam O'Reilly <L.P.OReilly@swansea.ac.uk>
2017-02-03 21:37:40 +0000

    Add method to get parent area from crag model

--- app/Crag.php
+++ app/Crag.php
@@ -9,4 +9,12 @@ class Crag extends Model
     protected $table = 'crags';
 
     protected $fillable = [];
+
+    /**
+     * Get the area that this crag is part of.
+     */
+    public function area()
+    {
+        return $this->belongsTo('App\Area', 'area_id', 'id');
+    }
 }
