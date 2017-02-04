773283279f53585ca33b67d3bc43a3e70d9bca31
Phil Reynolds <bitbucket@elasticparsley.uk>
2017-02-04 11:48:49 +0000

    added more areas for gower

--- database/seeds/AreasTableSeeder.php
+++ database/seeds/AreasTableSeeder.php
@@ -15,11 +15,59 @@ class AreasTableSeeder extends Seeder
         DB::table('areas')->delete();
 
         $a = new Area();
+        $a->name = "Wales";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "South Wales";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "Gower";
+        $a->save();
+
+        $a = new Area();
         $a->name = "North Gower";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "South Gower";
         $a->save();
         
         $a = new Area();
-        $a->name = "South Gower";
+        $a->name = "Rhosilli";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "Fall Bay to Mewslade";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "Thurba Head to Port Eynon";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "Oxwich";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "The Three Tors";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "Three Cliffs";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "Southgate and Pennard";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "Caswell to Mumbles";
+        $a->save();
+
+        $a = new Area();
+        $a->name = "Inland Crags";
         $a->save();
     }
 }
