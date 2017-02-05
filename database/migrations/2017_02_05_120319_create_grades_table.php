e3f0f4a7ed57b1fbfdc2f48bba98847ee55ca320
Phil Reynolds <Phil@elasticparsley.uk>
2017-02-05 14:07:32 +0000

    Create initial grades table migration.

---
+++ database/migrations/2017_02_05_120319_create_grades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table){
+           $table->increments('id');
+           $table->string('type');
+           $table->string('system');
+           $table->string('score');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('grades');
+    }
+}
