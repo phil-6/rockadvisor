309a2ccc22baf25e106fa316e0c23f37c0da3461
Phil Reynolds <Phil@elasticparsley.uk>
2017-02-09 23:11:12 +0000

    Route type seeder written for British Trad, French Sport, and V and Font Bouldering (Most common types)

--- database/seeds/RouteTypeTableSeeder.php
+++ database/seeds/RouteTypeTableSeeder.php
@@ -11,6 +11,30 @@ class RouteTypeTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB:Table('routeType')->delete();
+
+        //British Trad
+        $rt = new RouteType();
+        $rt->routeTypeSystemId = 1;
+        $rt->routeTypeNameId = 1;
+        $rt->save();
+
+        //French Sport
+        $rt = new RouteType();
+        $rt->routeTypeSystemId = 2;
+        $rt->routeTypeNameId = 2;
+        $rt->save();
+
+        //V Bouldering
+        $rt = new RouteType();
+        $rt->routeTypeSystemId = 3;
+        $rt->routeTypeNameId = 3;
+        $rt->save();
+
+        //Font Bouldering
+        $rt = new RouteType();
+        $rt->routeTypeSystemId = 4;
+        $rt->routeTypeNameId = 3;
+        $rt->save();
     }
 }
