8174b7e95a577f1125a3a66e5dbec3f1cfc667f2
Phil Reynolds <bitbucket@elasticparsley.uk>
2017-02-20 23:16:49 +0000

    Create Grades Controller - Currently mostly empty

---
+++ app/Http/Controllers/GradesController.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: philr
+ * Date: 20/02/2017
+ * Time: 23:11
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Access\Response;
+use Illuminate\Http\Request;
+use App\Grade;
+
+class GradesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function index()
+    // {
+    //     $areas = Area::orderBy('name')->get();
+    //     return view('areas.index', compact('areas'));
+    // }
+
+    public function api_index()
+    {
+//        $data = [];
+//        $grades = Grade::all();
+//        foreach ($grades as  $grade) {
+//            $record = ["id" => $grade->id, "name" => $grade->name];
+//            $data[] = $record;
+//        }
+//
+//        return response()->json($data);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function api_store(Request $request)
+    {
+
+        //Not sure if this will work
+        /*Area::create(array(
+            'name' => Input::get('name'),
+            'parentArea' => Input::get('parentArea')
+        ));*/
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Area  $area
+     * @return \Illuminate\Http\Response
+     */
+    // public function show(Area $area)
+    //
+    // }
+
+    public function api_show($grade)
+    {
+        return response()->json($grade);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function api_update($grade)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    // public function destroy($id)
+    // {
+    //     //
+    // }
+
+    public function api_destroy($grade)
+    {
+        //
+    }
+
+}
