81be6c9ab474114335b4ffbbc9191b61e1624388
Phil Reynolds <bitbucket@elasticparsley.uk>
2017-03-01 10:21:29 +0000

    Small Navbar changes - still no fix for mobile

--- public/views/main.php
+++ public/views/main.php
@@ -76,14 +76,14 @@
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
+                    data-target="#rockAdvisorNavbar">
                 <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
             </button>
             <a class="navbar-brand page-scroll" href="#page-top">Rock Advisor</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="rockAdvisorNavbar">
             <ul class="nav navbar-nav navbar-right">
                 <li>
                     <a class="page-scroll" href="#about">About</a>
