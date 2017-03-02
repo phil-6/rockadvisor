11db7dd7c5b69204c59885caae69b6e426d026f3
Phil Reynolds <bitbucket@elasticparsley.uk>
2017-03-02 21:57:32 +0000

    Fix bug with crag index not showing at /crags/

--- public/views/cragsIndex.php
+++ public/views/cragsIndex.php
@@ -34,9 +34,9 @@
 
     <!-- ANGULAR -->
     <!-- all angular resources will be loaded from the /public folder -->
-    <script src="angularApp/controllers/cragsCtrl.js"></script> <!-- load our controller -->
-    <script src="angularApp/services/cragService.js"></script> <!-- load our service -->
-    <script src="angularApp/app.js"></script> <!-- load our application -->
+    <script src="../angularApp/controllers/cragsCtrl.js"></script> <!-- load our controller -->
+    <script src="../angularApp/services/cragService.js"></script> <!-- load our service -->
+    <script src="../angularApp/app.js"></script> <!-- load our application -->
 
     <!-- Google Analytics -->
     <script>
