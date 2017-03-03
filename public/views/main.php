5b137dba01685dc68cc16fdf7f3fe3f95b4a6a23
Phil Reynolds <bitbucket@elasticparsley.uk>
2017-03-03 12:58:52 +0000

    interim commit sorting out front page - need to checkout a previous commit!

--- public/views/main.php
+++ public/views/main.php
@@ -132,5 +132,41 @@ This is going to be our master template
     <div ui-view></div>
 </div>
 
+<!--
+    ==========
+      Footer
+    ==========
+              -->
+
+<hr>
+<footer>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <p class="copyright text-muted">Copyright &copy; Rock Advisor 2017</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!--
+    ==========
+     Scripts
+    ==========
+              -->
+
+<!-- Bootstrap Core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="../js/scrollreveal/scrollreveal.min.js"></script>
+<script src="../js/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="../js/formValidation.js"></script>
+
+<!-- Theme JavaScript -->
+<script src="../js/creative-theme/creative.min.js"></script>
+
 </body>
 </html>
