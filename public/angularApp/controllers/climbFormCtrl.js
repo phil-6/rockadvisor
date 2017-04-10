5df602d073317d67b645df9c55bf50c8a7ab39e0
Phil Reynolds <bitbucket@elasticparsley.uk>
2017-04-10 13:19:04 +0100

    Display climbs at crag when adding new climb.

--- public/angularApp/controllers/climbFormCtrl.js
+++ public/angularApp/controllers/climbFormCtrl.js
@@ -6,6 +6,7 @@ angular.module('climbFormCtrl', [])
     .controller('climbFormController', function ($scope, $http, ClimbFactory, CragFactory, GradeFactory, $timeout) {
 
         $scope.formData = {};
+        $scope.showCurrentClimbs = false;
         $scope.submitButtonHidden = false;
         $scope.progressMessageShowing = false;
         $scope.resultShowing = false;
@@ -14,17 +15,33 @@ angular.module('climbFormCtrl', [])
         $scope.dateregex = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
         //console.log("ctrl-here");
 
+        /*Get All Crags*/
         $scope.cragsData = {};
         CragFactory.get()
             .success(function (data2) {
                 $scope.cragsData = data2;
             });
 
+        /*Get All Grades*/
         $scope.gradesData = {};
         GradeFactory.get()
             .success(function (data3) {
                 $scope.gradesData = data3;
             });
+
+        /*Get Climbs at Crag*/
+        $scope.climbsAtCrag = {};
+        $scope.cragSelected = function () {
+            CragFactory.getClimbs($scope.formData.crag)
+                .success(function (data4) {
+                    $scope.climbsAtCrag = data4;
+                });
+            if ($scope.climbsAtCrag) {
+                $scope.showCurrentClimbs = true;
+            }
+
+
+        };
 
 
         $scope.processClimbForm = function (isValid) {
